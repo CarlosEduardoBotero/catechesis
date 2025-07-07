ce3f5544d7b9e8d7fed4e65faf0e13d4b84efd5c
Carlos Botero <cboteroviloria@buenosaires.gob.ar>
2025-07-07 16:29:38 -0400

    updated packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,5 +7,8 @@ export default defineConfig({
     defaultLocale: "en",
     locales: ["es", "en", "pt"],
   },
-  integrations: [tailwind()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
