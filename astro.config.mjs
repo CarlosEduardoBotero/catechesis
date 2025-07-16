5c91f0264eb750c3ccc36125d4d8b41f0860807b
Carlos Botero <boterocev@gmail.com>
2025-07-16 15:59:01 -0400

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
@@ -11,4 +13,17 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "http://injesuslikeness.com",
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en", // All urls that don't contain `es` or `pt` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: "en-US",
+          es: "es-MX",
+          pt: "pt-BR",
+        },
+      },
+    }),
+  ],
 });
